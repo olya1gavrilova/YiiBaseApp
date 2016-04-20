c1a5d15904a08fb80275372924b86a0159d2eaa4
Olya Gavrilova <login.gavrilova@yandex.ru>
2016-04-20 18:04:59 +0300

    views\user\view update

--- views/user/view.php
+++ views/user/view.php
@@ -1,38 +1,121 @@
 <?php
 
 use yii\helpers\Html;
-use yii\widgets\DetailView;
+use yii\grid\GridView;
 
-
+use app\models\User;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\User */
+/* @var $searchModel app\models\UserSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
+$this->title = 'Пользователь '.$model->username;
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-view">
 
-   <h1><?=$model->title?></h1>
-        <?php if($ok):?>
-            <div class="alert alert-success" role="alert">Ваш комментарий получен и будет опубликован после подтверждения модератором</div>
-        <?php endif?>
+    <?php if (Yii::$app->user->can('enter-admin')):?>
+        <?=Html::a('Управление ролями', '/roles/index', ['class' => 'btn btn-info'])?>
+     <?php endif?>
 
 
-        <br />
-        <?=$model->content?>
-        <br /><br /><br />
-        <hr />
-        <h4>Комментарии</h4>
-        <br />
-        <?= LinkPager::widget(['pagination' => $pagination]) ?>
-        <?php foreach($comments as $comment):?>
-            <br />
-            <b><?=$comment->auth_nick?></b> написал <b><?=$comment->date?>:</b><br />
-            <?=$comment->text?>
-            <br />
-            <hr />
-        <?php endforeach?>
-            <?=Html::a('Добавить комментарий','index' ,['class'=>'btn btn-info']);?>
+     <?php if (Yii::$app->user->can('delete-post') || Yii::$app->user->can('update-post')):?>
+        <?=Html::a('Управление постами', '/post/index', ['class' => 'btn btn-info'])?>
+    <?php endif?>
+
+
+    <?php if (Yii::$app->user->can('comment-list')):?>
+        <?=Html::a('Управление комментариями', '/comments/index', ['class' => 'btn btn-info '])?>
+     <?php endif?>
+
+      <?php if (Yii::$app->user->can('category-create')):?>
+        <?=Html::a('Создать категорию', '/category/create', ['class' => 'btn btn-info'])?>
+     <?php endif?>
+
+     <?php if (Yii::$app->user->can('user-update')):?>
+        <?=Html::a('Все пользователи', '/user/index', ['class' => 'btn btn-info'])?>
+     <?php endif?>
+
+    
+
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <table class='table table-bordered'>
+        <tr>
+            <td>Имя</td>
+            <td><?=$model->first_name?></td>
+        </tr>
+        <tr>
+            <td>Фамилия</td>
+            <td><?=$model->last_name?></td>
+        </tr>
+        <tr>
+            <td>Имя</td>
+            <td><?=$model->username?></td>
+        </tr>
+        <tr>
+            <td>E-mail</td>
+            <td></td>
+        </tr>
+    
+    <?php if (Yii::$app->user->can('role-update')):?>
+        <tr>
+            <td>Статус</td>
+            <td>
+            
+
+            <?=$role->item_name?>
+           
+            </td>
+        </tr>
+    <?php endif?>
+    </table>
+    <?=Html::a('Редактировать данные', ['update', 'id' => $model->id], ['class' => 'btn btn-primary'])?>
+    <br /><hr />
+
+
+    <h2>Посты</h2>
+    <table class='table table-bordered'>
+             <tr>
+                <td>Дата</td>
+                <td>Название</td>
+                <td>Анонс</td>
+                <td></td>
+            </tr>
+        <?php foreach($posts as $post):?>
+           <tr>
+               
+                    <td><?=$post->publish_date?></td>
+                    <td><?=Html::a($post->title,['../post/view', 'id' => $post->id])?></td>
+                    <td><?=$post->anons?></td>
+                
+                <td><?=Html::a('Редактировать',['../post/update', 'id' => $post->id], ['class' => 'btn btn-info btn-xs'])?></td>
+            </tr>
+         <?php endforeach?>  
+    </table>
+     <?=Html::a('Все посты '.$model->username, ['../post/all', 'id' => $model->id], ['class' => 'btn btn-primary'])?>
+    <br /><hr />
+
+
+    <h2>Комментарий</h2>
+    <table class='table table-bordered'>
+             <tr>
+                <td>Дата</td>
+                <td>Пост</td>
+                <td>Текст комментария</td>
+                <td></td>
+            </tr>
+            <?php foreach($comments as $comment):?>
+           <tr>
+               
+                    <td><?=$comment->date?></td>
+                    <td><?=Html::a($comment->title,['../post/view', 'id' => $comment->post_id])?></td>
+                    <td><?=$comment->short_text?></td>
+                
+                <td><?=Html::a('Редактировать',['../post/update', 'id' => $post->id], ['class' => 'btn btn-info btn-xs'])?></td>
+            </tr>
+         <?php endforeach?>  
+
+    </table>
+     <?=Html::a('Все комментарии '.$model->username, ['../comments/view', 'id' => $model->id], ['class' => 'btn btn-primary'])?>
 </div>
